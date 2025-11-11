eef4c3f09645e10b3184d14068606f63bda3a401
wswalker87 <scott@wscottwalker.com>
2025-11-10 19:44:28 -0600

    Solution with Scott Walker and Nicholas Van Doren

--- fibonacci.js
+++ fibonacci.js
@@ -2,8 +2,6 @@ function fibonacci(num) {
   // create array of nums with range 0-num
   // Fn = (Fn-1) + (Fn-2)
   let fullArray = [0, 1]
-  // count = 0
-
   for (i = 0; i <= num; i++) {
     let tempNum = fullArray[i] + fullArray[i + 1]
     fullArray.push(tempNum)
