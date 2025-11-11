cd1ba2d798c9de975d3828924f85bdeafcd946ef
wswalker87 <scott@wscottwalker.com>
2025-11-10 19:43:28 -0600

    Solution with Scott Walker and Nicholas Van Doren

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-  return 0;
-}
+  // create array of nums with range 0-num
+  // Fn = (Fn-1) + (Fn-2)
+  let fullArray = [0, 1]
+  // count = 0
 
+  for (i = 0; i <= num; i++) {
+    let tempNum = fullArray[i] + fullArray[i + 1]
+    fullArray.push(tempNum)
+  }
+  return fullArray[num];
+}
+console.log(fibonacci(10))
 module.exports = fibonacci;
